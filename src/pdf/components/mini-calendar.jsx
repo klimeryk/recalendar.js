cdbffea38772b6c3c00d31f3602c49c0a08821db
Igor Klimer <github@gpg.klimer.eu>
2021-12-06 18:03:13 +0000

    Dynamic rendering of mini calendar

--- src/pdf/components/mini-calendar.jsx
+++ src/pdf/components/mini-calendar.jsx
@@ -50,58 +50,50 @@ class MiniCalendar extends React.Component {
 		);
 	}
 
+	renderMonth() {
+		let currentWeek = this.props.date.startOf( 'month' ).startOf( 'week' );
+		const endDate = this.props.date.endOf( 'month' );
+		const weekRows = [];
+		while ( currentWeek.isBefore( endDate ) ) {
+			weekRows.push( this.renderWeek( currentWeek ) );
+			currentWeek = currentWeek.add( 1, 'week' );
+		}
+
+		return <>{weekRows}</>;
+	}
+
+	renderWeek( week ) {
+		const { day } = this.styles;
+		const days = [];
+		for ( let i = 0; i < 7; i++ ) {
+			const currentDay = week.add( i, 'days' );
+			days.push(
+				<Text key={ i } style={ day }>
+					{currentDay.date()}
+				</Text>,
+			);
+		}
+		/*
+		<Link src="#page10" style={ day }>
+			1
+		</Link>
+		*/
+
+		return (
+			<View style={ this.styles.week }>
+				<Text style={ day }>{week.isoWeek()}</Text>
+				{days}
+				<Text style={ day }>R</Text>
+			</View>
+		);
+	}
+
 	render() {
 		const { month, week, day } = this.styles;
 		return (
 			<View style={ month }>
 				{this.renderHeader()}
-				<View style={ week }>
-					<Text style={ day }></Text>
-					<Text style={ day }></Text>
-					<Text style={ day }></Text>
-					<Link src="#page10" style={ day }>
-						1
-					</Link>
-					<Text style={ day }>2</Text>
-					<Text style={ day }>3</Text>
-					<Text style={ day }>4</Text>
-				</View>
-				<View style={ week }>
-					<Text style={ day }>5</Text>
-					<Text style={ day }>6</Text>
-					<Text style={ day }>7</Text>
-					<Text style={ day }>8</Text>
-					<Text style={ day }>9</Text>
-					<Text style={ day }>10</Text>
-					<Text style={ day }>11</Text>
-				</View>
-				<View style={ week }>
-					<Text style={ day }>12</Text>
-					<Text style={ day }>13</Text>
-					<Text style={ day }>14</Text>
-					<Text style={ day }>15</Text>
-					<Text style={ day }>16</Text>
-					<Text style={ day }>17</Text>
-					<Text style={ day }>18</Text>
-				</View>
-				<View style={ week }>
-					<Text style={ day }>19</Text>
-					<Text style={ day }>20</Text>
-					<Text style={ day }>21</Text>
-					<Text style={ day }>22</Text>
-					<Text style={ day }>23</Text>
-					<Text style={ day }>24</Text>
-					<Text style={ day }>25</Text>
-				</View>
-				<View style={ week }>
-					<Text style={ day }>26</Text>
-					<Text style={ day }>27</Text>
-					<Text style={ day }>28</Text>
-					<Text style={ day }>29</Text>
-					<Text style={ day }>30</Text>
-					<Text style={ day }>31</Text>
-					<Text style={ day }></Text>
-				</View>
+				{this.renderMonth()}
 			</View>
 		);
 	}
