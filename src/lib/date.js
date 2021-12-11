f2abdad6fccc79db3e16c3ce40fcedfa6b6f821a
Igor Klimer <github@gpg.klimer.eu>
2021-12-11 19:09:54 +0000

    Remove source files for the GH Pages branch

--- src/lib/date.js
+++
@@ -1,7 +0,0 @@
-import dayjs from 'dayjs';
-
-export function getWeekdays() {
-	const weekdays = dayjs.weekdaysMin();
-	const firstDayOfWeek = dayjs.localeData().firstDayOfWeek();
-	return [ ...weekdays.slice( firstDayOfWeek ), ...weekdays.slice( 0, firstDayOfWeek ) ];
-}
