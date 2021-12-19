klimeryk/recalendar.js

commit 28b2f7219049619d9a99d883db35f346ea6563ea

Extract PDF preview card to a separate component

src/components/pdf-preview-card.js
@@ -1,0 +1,89 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
+import Stack from 'react-bootstrap/Stack';
+import { withTranslation } from 'react-i18next';
+
+import PdfPreview from 'components/pdf-preview';
+
+class PdfPreviewCard extends React.PureComponent {
+	renderPdfPreview() {
+		const { blobUrl, isGeneratingPdf, isGeneratingPreview, t } = this.props;
+		return (
+			<Stack direction="vertical" gap={ 3 } className="h-100">
+				<PdfPreview
+					blobUrl={ blobUrl }
+					title={ t( 'configuration.preview.viewer-title' ) }
+				/>
+				<Button
+					variant="secondary"
+					disabled={ isGeneratingPreview || isGeneratingPdf }
+					onClick={ this.handleDownload }
+				>
+					{isGeneratingPdf ? (
+						<>
+							<Spinner
+								as="span"
+								animation="border"
+								size="sm"
+								role="status"
+								aria-hidden="true"
+								className="me-1"
+							/>
+							Generating full calendar - this could take a minute or more...
+						</>
+					) : (
+						t( 'configuration.button.download' )
+					)}
+				</Button>
+			</Stack>
+		);
+	}
+
+	renderNoPreview() {
+		if ( this.props.isGeneratingPreview ) {
+			return (
+				<div className="h-100 d-flex align-items-center justify-content-center">
+					<Spinner
+						animation="border"
+						role="status"
+						size="sm"
+						className="me-1"
+					/>
+					Generating preview, please wait - it can take a minute.
+				</div>
+			);
+		}
+		return (
+			<Stack
+				direction="vertical"
+				className="h-100 d-flex align-items-center justify-content-center"
+			>
+				<p className="lead">
+					Use the configuration form to create your personalized calendar.
+				</p>
+				<p>The preview will appear here.</p>
+			</Stack>
+		);
+	}
+
+	render() {
+		const { blobUrl, isGeneratingPreview } = this.props;
+		if ( blobUrl && ! isGeneratingPreview ) {
+			return this.renderPdfPreview();
+		}
+
+		return this.renderNoPreview();
+	}
+}
+
+PdfPreviewCard.propTypes = {
+	blobUrl: PropTypes.string,
+	isGeneratingPdf: PropTypes.bool.isRequired,
+	isGeneratingPreview: PropTypes.bool.isRequired,
+	onDownload: PropTypes.func.isRequired,
+	t: PropTypes.func.isRequired,
+};
+
+export default withTranslation( 'app' )( PdfPreviewCard );

src/App.js
@@ -16,7 +16,7 @@ import { withTranslation } from 'react-i18next';
 
 import PdfWorker from './worker/pdf.worker.js'; // eslint-disable-line import/default
 
-import PdfPreview from 'components/pdf-preview';
+import PdfPreviewCard from 'components/pdf-preview-card';
 import AboutModal from 'configuration-form/about-modal';
 import ItemsList from 'configuration-form/items-list';
 import Itinerary from 'configuration-form/itinerary';
@@ -412,69 +412,7 @@ class App extends React.PureComponent {
 		);
 	}
 
-	renderPdfPreview() {
-		const { t } = this.props;
-		const { isGeneratingPdf, isGeneratingPreview, blobUrl } = this.state;
-		return (
-			<Stack direction="vertical" gap={ 3 } className="h-100">
-				<PdfPreview
-					blobUrl={ blobUrl }
-					title={ t( 'configuration.preview.viewer-title' ) }
-				/>
-				<Button
-					variant="secondary"
-					disabled={ isGeneratingPreview || isGeneratingPdf }
-					onClick={ this.handleDownload }
-				>
-					{isGeneratingPdf ? (
-						<>
-							<Spinner
-								as="span"
-								animation="border"
-								size="sm"
-								role="status"
-								aria-hidden="true"
-								className="me-1"
-							/>
-							Generating full calendar - this could take a minute or more...
-						</>
-					) : (
-						t( 'configuration.button.download' )
-					)}
-				</Button>
-			</Stack>
-		);
-	}
-
-	renderNoPreview() {
-		if ( this.state.isGeneratingPreview ) {
-			return (
-				<div className="h-100 d-flex align-items-center justify-content-center">
-					<Spinner
-						animation="border"
-						role="status"
-						size="sm"
-						className="me-1"
-					/>
-					Generating preview, please wait - it can take a minute.
-				</div>
-			);
-		}
-		return (
-			<Stack
-				direction="vertical"
-				className="h-100 d-flex align-items-center justify-content-center"
-			>
-				<p className="lead">
-					Use the configuration form to create your personalized calendar.
-				</p>
-				<p>The preview will appear here.</p>
-			</Stack>
-		);
-	}
-
 	render() {
-		const { blobUrl, isGeneratingPreview, showAboutModal } = this.state;
 		const { t } = this.props;
 
 		return (
@@ -482,7 +420,7 @@ class App extends React.PureComponent {
 				<Row className="h-100">
 					<AboutModal
 						onHide={ this.handleHideAboutModal }
-						show={ showAboutModal }
+						show={ this.state.showAboutModal }
 					/>
 					<Col className="h-100 overflow-auto">
 						{this.renderConfigurationForm()}
@@ -491,9 +429,12 @@ class App extends React.PureComponent {
 						<Card className="h-100">
 							<Card.Header>{t( 'preview.title' )}</Card.Header>
 							<Card.Body>
-								{blobUrl && ! isGeneratingPreview
-									? this.renderPdfPreview()
-									: this.renderNoPreview()}
+								<PdfPreviewCard
+									blobUrl={ this.state.blobUrl }
+									isGeneratingPdf={ this.state.isGeneratingPdf }
+									isGeneratingPreview={ this.state.isGeneratingPreview }
+									onDownload={ this.handleDownload }
+								/>
 							</Card.Body>
 						</Card>
 					</Col>

commit 5f408c5748b63d5c68f9dcb9e7d09690f78ac749

Fix generating the full PDF
Broke it in 28b2f7219049619d9a99d883db35f346ea6563ea

src/components/pdf-preview-card.js
@@ -9,14 +9,15 @@ import PdfPreview from 'components/pdf-preview';
 
 class PdfPreviewCard extends React.PureComponent {
 	renderPdfPreview() {
-		const { blobUrl, isGeneratingPdf, isGeneratingPreview, t } = this.props;
+		const { blobUrl, isGeneratingPdf, isGeneratingPreview, onDownload, t } =
+			this.props;
 		return (
 			<Stack direction="vertical" gap={ 3 } className="h-100">
 				<PdfPreview blobUrl={ blobUrl } title={ t( 'preview.viewer-title' ) } />
 				<Button
 					variant="secondary"
 					disabled={ isGeneratingPreview || isGeneratingPdf }
-					onClick={ this.handleDownload }
+					onClick={ onDownload }
 				>
 					{isGeneratingPdf ? (
 						<>