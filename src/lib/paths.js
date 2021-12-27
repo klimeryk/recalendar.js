d7b7cb928aa275c5dfa4e7f3699f81e7067892e2
Igor Klimer <github@gpg.klimer.eu>
2021-12-27 20:36:52 +0000

    Better link for creating your own calendar

--- src/lib/paths.js
+++ src/lib/paths.js
@@ -3,6 +3,6 @@ export const RECALENDAR_JS_GITHUB =
 export const RECALENDAR_PHP_GITHUB = 'https://github.com/klimeryk/recalendar/';
 
 export const HOME_PATH = '/';
-export const CONFIGURATOR_PATH = '/configuration';
+export const CONFIGURATOR_PATH = '/create';
 export const FEATURES_PATH = '/features';
 export const FAQ_PATH = '/faq';
