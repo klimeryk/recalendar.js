klimeryk/recalendar.js

commit f6529d34735d21e8b3161d3932048f87714b1562

Revamp of special dates
- Adds ability to specify type of special date.
- Changes the internal data structures so that it's easier to remove
  individual items. And in the future, maybe add editing?
- Simplify the logic and code based on the above improvements.

Fixes #31

src/configuration-form/configuration-selector.jsx
@@ -184,7 +184,8 @@ class ConfigurationSelector extends React.Component {
 			status: STATUS_SUCCESS,
 		} );
 
-		this.props.onConfigChange( convertConfigToCurrentVersion( attachment ) );
+		const newConfig = new PdfConfig( convertConfigToCurrentVersion( attachment ) );
+		this.props.onConfigChange( newConfig );
 	};
 
 	renderStatusMessage() {

src/app.css
@@ -30,3 +30,7 @@ button:not(:disabled).grab-handle {
 	cursor: move;
 	padding-left: 0;
 }
+
+.flex-basis-fit-content {
+	flex-basis: fit-content !important;
+}

src/configuration-form/special-dates.jsx
@@ -5,24 +5,30 @@ import Accordion from 'react-bootstrap/Accordion';
 import Alert from 'react-bootstrap/Alert';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import InputGroup from 'react-bootstrap/InputGroup';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Stack from 'react-bootstrap/Stack';
 import { withTranslation } from 'react-i18next';
 
-const DATE_FORMAT = 'DD-MM';
+export const HOLIDAY_DAY_TYPE = 'holiday';
+export const EVENT_DAY_TYPE = 'event';
+
+export function isHoliday( { type } ) {
+	return type === HOLIDAY_DAY_TYPE;
+}
+export function isEvent( { type } ) {
+	return type === EVENT_DAY_TYPE;
+}
+
+export const DATE_FORMAT = 'MM-DD';
 
 class SpecialDates extends React.Component {
 	state = {
 		date: '',
 		value: '',
-	};
-
-	compareSpecialDates = ( a, b ) => {
-		const dateA = dayjs( a, DATE_FORMAT );
-		const dateB = dayjs( b, DATE_FORMAT );
-		return dateA.isAfter( dateB ) ? 1 : -1;
+		type: EVENT_DAY_TYPE,
 	};
 
 	onChange = ( event ) => {
@@ -33,40 +39,66 @@ class SpecialDates extends React.Component {
 	onAddClick = ( event ) => {
 		const date = dayjs( this.state.date, 'YYYY-MM-DD' );
 		const key = date.format( DATE_FORMAT );
-		this.props.onAdd( key, this.state.value );
+		const { value, type } = this.state;
+		this.props.onAdd( { date: key, value, type } );
 		this.setState( { date: '', value: '' } );
 	};
 
-	renderItem = ( key ) => {
-		const values = this.props.items[ key ];
-		const date = dayjs( key, DATE_FORMAT );
-		return (
-			<ListGroup.Item key={ key }>
-				<Stack direction="horizontal" gap={ 3 }>
-					<b className="special-date">{date.format( 'MMMM DD' )}</b>
-					<div>
-						{values.map( ( value, index ) => (
-							<div key={ key + index }>{value}</div>
-						) )}
-					</div>
-					{this.renderRemoveButton( key )}
-				</Stack>
-			</ListGroup.Item>
-		);
-	};
-
-	renderItems( items ) {
-		return <ListGroup>{items.map( this.renderItem )}</ListGroup>;
+	getGroupedItems() {
+		return this.props.items.reduce( ( itemsSoFar, item ) => {
+			if ( ! itemsSoFar[ item.date ] ) {
+				itemsSoFar[ item.date ] = [];
+			}
+			itemsSoFar[ item.date ].push( item );
+			return itemsSoFar;
+		}, {} );
 	}
 
-	renderRemoveButton( key ) {
+	renderItem( groupedItems ) {
+		const ItemGroup = ( key ) => {
+			const { t } = this.props;
+			const items = groupedItems[ key ];
+			const date = dayjs( key, DATE_FORMAT );
+			return (
+				<ListGroup.Item key={ key }>
+					<Stack direction="horizontal" gap={ 3 }>
+						<b className="special-date">{date.format( 'MMMM DD' )}</b>
+						<ListGroup variant="flush" className="w-100">
+							{items.map( ( { id, value, type }, index ) => (
+								<ListGroup.Item key={ id } className="ps-0 pe-0">
+									<Stack direction="horizontal" gap={ 3 }>
+										<span>
+											<strong>
+												{t( 'configuration.special-dates.type.' + type )}:{' '}
+											</strong>
+											{value}
+										</span>
+										{this.renderRemoveButton( id )}
+									</Stack>
+								</ListGroup.Item>
+							) )}
+						</ListGroup>
+					</Stack>
+				</ListGroup.Item>
+			);
+		};
+		return ItemGroup;
+	}
+
+	renderItems( groupedItems ) {
+		const keys = Object.keys( groupedItems ).sort();
+		return <ListGroup>{keys.map( this.renderItem( groupedItems ) )}</ListGroup>;
+	}
+
+	renderRemoveButton( id ) {
 		const { onRemove, t } = this.props;
 		return (
 			<Button
 				className="ms-auto"
 				variant="outline-danger"
 				onClick={ onRemove }
-				data-key={ key }
+				data-field="specialDates"
+				data-id={ id }
 			>
 				{t( 'configuration.special-dates.button.remove' )}
 			</Button>
@@ -74,24 +106,25 @@ class SpecialDates extends React.Component {
 	}
 
 	render() {
-		const { date, value } = this.state;
-		const { items, t } = this.props;
-		const keys = Object.keys( items ).sort( this.compareSpecialDates );
+		const { date, value, type } = this.state;
+		const { t } = this.props;
+		const groupedItems = this.getGroupedItems();
+		const numberOfItems = Object.keys( groupedItems ).length;
 		return (
 			<Accordion.Item eventKey="specialDates">
 				<Accordion.Header>
 					<Stack direction="horizontal" className="w-100">
 						{t( 'configuration.special-dates.title' )}
 						<Badge bg="info" className="ms-auto me-3">
-							{keys.length}
+							{numberOfItems}
 						</Badge>
 					</Stack>
 				</Accordion.Header>
 				<Accordion.Body>
 					<p>{t( 'configuration.special-dates.description' )}</p>
 					<Stack gap={ 2 }>
-						{keys ? (
-							this.renderItems( keys )
+						{numberOfItems > 0 ? (
+							this.renderItems( groupedItems )
 						) : (
 							<Alert variant="secondary" className="mb-0">
 								{t( 'configuration.special-dates.empty' )}
@@ -100,6 +133,20 @@ class SpecialDates extends React.Component {
 					</Stack>
 					<Stack direction="horizontal" className="mt-3">
 						<InputGroup>
+							<Form.Select
+								className="flex-grow-0 flex-basis-fit-content"
+								value={ type }
+								data-field="type"
+								onChange={ this.onChange }
+								aria-label="Default select example"
+							>
+								<option value={ EVENT_DAY_TYPE }>
+									{t( 'configuration.special-dates.type.' + EVENT_DAY_TYPE )}
+								</option>
+								<option value={ HOLIDAY_DAY_TYPE }>
+									{t( 'configuration.special-dates.type.' + HOLIDAY_DAY_TYPE )}
+								</option>
+							</Form.Select>
 							<FormControl
 								className="flex-grow-0 date-field"
 								value={ date }
@@ -129,7 +176,7 @@ class SpecialDates extends React.Component {
 }
 
 SpecialDates.propTypes = {
-	items: PropTypes.object.isRequired,
+	items: PropTypes.array.isRequired,
 	onAdd: PropTypes.func.isRequired,
 	onRemove: PropTypes.func.isRequired,
 	t: PropTypes.func.isRequired,

src/configuration.jsx
@@ -313,20 +313,9 @@ class Configuration extends React.PureComponent {
 		this.setState( { dayItineraries: newItineraries } );
 	};
 
-	handleSpecialDateAdd = ( key, value ) => {
-		const newSpecialDates = Object.assign( {}, this.state.specialDates );
-		if ( ! newSpecialDates[ key ] ) {
-			newSpecialDates[ key ] = [];
-		}
-
-		newSpecialDates[ key ].push( value );
-		this.setState( { specialDates: newSpecialDates } );
-	};
-
-	handleSpecialDateRemove = ( event ) => {
-		const key = event.target.dataset.key;
-		const newSpecialDates = Object.assign( {}, this.state.specialDates );
-		delete newSpecialDates[ key ];
+	handleSpecialDateAdd = ( newSpecialDate ) => {
+		const newSpecialDates = [ ...this.state.specialDates ];
+		newSpecialDates.push( wrapWithId( newSpecialDate ) );
 		this.setState( { specialDates: newSpecialDates } );
 	};
 
@@ -501,7 +490,7 @@ class Configuration extends React.PureComponent {
 					<SpecialDates
 						items={ this.state.specialDates }
 						onAdd={ this.handleSpecialDateAdd }
-						onRemove={ this.handleSpecialDateRemove }
+						onRemove={ this.handleItemRemove }
 					/>
 					<ToggleAccordionItem
 						id="isMonthOverviewEnabled"

src/lib/config-compat.js
@@ -1,18 +1,26 @@
-import PdfConfig, {
+import dayjs from 'dayjs';
+
+import {
+	DATE_FORMAT as SPECIAL_DATES_DATE_FORMAT,
+	EVENT_DAY_TYPE,
+} from 'configuration-form/special-dates';
+import {
 	CONFIG_VERSION_1,
+	CONFIG_VERSION_2,
 	CONFIG_CURRENT_VERSION,
 } from 'pdf/config';
 
 export function convertConfigToCurrentVersion( config ) {
-	const configWithDefaults = new PdfConfig( config );
-
-	switch ( configWithDefaults.version ) {
+	switch ( config.version ) {
 		case CONFIG_VERSION_1:
-			return convertFromVersion1( configWithDefaults );
+			return convertFromVersion2( convertFromVersion1( config ) );
+
+		case CONFIG_VERSION_2:
+			return convertFromVersion2( config );
 
 		case CONFIG_CURRENT_VERSION:
 		default:
-			return configWithDefaults;
+			return config;
 	}
 }
 
@@ -20,5 +28,24 @@ function convertFromVersion1( config ) {
 	config.dayItineraries = config.dayItineraries.map( ( dayItinerary ) =>
 		Object.assign( { isEnabled: true }, dayItinerary ),
 	);
+	return config;
+}
+
+function convertFromVersion2( config ) {
+	const oldDateKeyFormat = 'DD-MM';
+	const newSpecialDates = [];
+	Object.keys( config.specialDates || {} ).forEach( ( dateKey ) => {
+		const newDateKey = dayjs( dateKey, oldDateKeyFormat ).format(
+			SPECIAL_DATES_DATE_FORMAT,
+		);
+		newSpecialDates.push(
+			...config.specialDates[ dateKey ].map( ( specialDate ) => ( {
+				date: newDateKey,
+				value: specialDate,
+				type: EVENT_DAY_TYPE,
+			} ) ),
+		);
+	} );
+	config.specialDates = newSpecialDates;
 	return config;
 }

src/pdf/components/header.jsx
@@ -89,9 +89,9 @@ class Header extends React.PureComponent {
 
 		return (
 			<View style={ this.styles.specialItems }>
-				{this.props.specialItems.map( ( text, index ) => (
+				{this.props.specialItems.map( ( { value }, index ) => (
 					<Text key={ index } style={ this.styles.specialItem }>
-						» {text}
+						» {value}
 					</Text>
 				) )}
 			</View>

src/pdf/components/mini-calendar.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { withTranslation } from 'react-i18next';
 
+import { isHoliday, isEvent } from 'configuration-form/special-dates';
 import { getWeekdays, getWeekendDays, getWeekNumber } from 'lib/date';
 import PdfConfig from 'pdf/config';
 import {
@@ -77,9 +78,9 @@ class MiniCalendar extends React.Component {
 			border: '1 solid #CCC',
 		},
 		weekendDay: {
-			fontWeight: 'bold',
+			fontWeight: 1000,
 		},
-		specialDay: {
+		eventDay: {
 			border: '1px solid #555',
 		},
 		otherMonthDay: {
@@ -221,7 +222,13 @@ class MiniCalendar extends React.Component {
 
 			const specialDateKey = currentDay.format( 'DD-MM' );
 			if ( config.specialDates[ specialDateKey ] ) {
-				dayStyles.push( this.styles.specialDay );
+				if ( config.specialDates[ specialDateKey ].some( isEvent ) ) {
+					dayStyles.push( this.styles.eventDay );
+				}
+
+				if ( config.specialDates[ specialDateKey ].some( isHoliday ) ) {
+					dayStyles.push( this.styles.weekendDay );
+				}
 			}
 
 			days.push(

src/pdf/config.js
@@ -2,6 +2,10 @@ import dayjs from 'dayjs';
 import { t } from 'i18next';
 
 import { ITINERARY_ITEM, ITINERARY_LINES } from 'configuration-form/itinerary';
+import {
+	HOLIDAY_DAY_TYPE,
+	EVENT_DAY_TYPE,
+} from 'configuration-form/special-dates';
 import { wrapWithId } from 'lib/id-utils';
 import { LATO } from 'pdf/lib/fonts';
 
@@ -28,7 +32,8 @@ const CONFIG_FIELDS = [
 export const CONFIG_FILE = 'config.json';
 export const CONFIG_VERSION_1 = 'v1';
 export const CONFIG_VERSION_2 = 'v2';
-export const CONFIG_CURRENT_VERSION = CONFIG_VERSION_2;
+export const CONFIG_VERSION_3 = 'v3';
+export const CONFIG_CURRENT_VERSION = CONFIG_VERSION_3;
 
 export function hydrateFromObject( object ) {
 	return CONFIG_FIELDS.reduce(
@@ -100,18 +105,38 @@ class PdfConfig {
 		];
 		// See https://github.com/diegomura/react-pdf/issues/2006
 		this.pageSize = [ 445, 592 ]; // [ '157mm', '209mm' ];
-		this.specialDates = {
-			'01-01': [
-				t( 'special-dates.example1', { ns: 'config' } ),
-				t( 'special-dates.example2', { ns: 'config' } ),
-			],
-			'03-01': [ t( 'special-dates.example3', { ns: 'config' } ) ],
-			'13-01': [
-				t( 'special-dates.example4', { ns: 'config' } ),
-				t( 'special-dates.example5', { ns: 'config' } ),
-			],
-			'14-01': [ t( 'special-dates.example6', { ns: 'config' } ) ],
-		};
+		this.specialDates = [
+			{
+				date: '01-01',
+				value: t( 'special-dates.example1', { ns: 'config' } ),
+				type: HOLIDAY_DAY_TYPE,
+			},
+			{
+				date: '01-01',
+				value: t( 'special-dates.example2', { ns: 'config' } ),
+				type: HOLIDAY_DAY_TYPE,
+			},
+			{
+				date: '01-03',
+				value: t( 'special-dates.example3', { ns: 'config' } ),
+				type: HOLIDAY_DAY_TYPE,
+			},
+			{
+				date: '01-13',
+				value: t( 'special-dates.example4', { ns: 'config' } ),
+				type: EVENT_DAY_TYPE,
+			},
+			{
+				date: '01-13',
+				value: t( 'special-dates.example5', { ns: 'config' } ),
+				type: HOLIDAY_DAY_TYPE,
+			},
+			{
+				date: '01-14',
+				value: t( 'special-dates.example6', { ns: 'config' } ),
+				type: EVENT_DAY_TYPE,
+			},
+		];
 
 		if ( Object.keys( configOverrides ).length !== 0 ) {
 			Object.assign( this, configOverrides );
@@ -124,6 +149,7 @@ class PdfConfig {
 		const fieldsRequiringUniqueIds = [
 			'habits',
 			'monthItinerary',
+			'specialDates',
 			'todos',
 			'weekRetrospectiveItinerary',
 		];

commit 5a063264c6214f72fcaa4e8a71ceefa336a28440

Fix special dates type in templates
Regression introduced in https://github.com/klimeryk/recalendar.js/commit/f6529d34735d21e8b3161d3932048f87714b1562
Fixes #97

src/configuration-form/configuration-selector.jsx
@@ -84,7 +84,7 @@ class ConfigurationSelector extends React.Component {
 				break;
 
 			case TEMPLATE_BLANK:
-				configOverrides.specialDates = {};
+				configOverrides.specialDates = [];
 				configOverrides.habits = [];
 				configOverrides.monthItinerary = [];
 				configOverrides.todos = [];
@@ -101,7 +101,7 @@ class ConfigurationSelector extends React.Component {
 				break;
 
 			case TEMPLATE_MINIMALISTIC:
-				configOverrides.specialDates = {};
+				configOverrides.specialDates = [];
 				configOverrides.habits = [];
 				configOverrides.isMonthOverviewEnabled = false;
 				configOverrides.monthItinerary = [];