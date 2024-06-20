46f30f3d5992dd7a1dde2492f1f32f86c2ba4042
Igor Klimer <github@gpg.klimer.eu>
2024-06-20 22:38:54 +0000

    Prepare production build config for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,37 @@
-import { defineConfig } from 'vite'
-import i18nextLoader from 'vite-plugin-i18next-loader'
-import commonjs from 'vite-plugin-commonjs'
-import react from '@vitejs/plugin-react'
 import path from 'path';
 
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import commonjs from 'vite-plugin-commonjs';
+import i18nextLoader from 'vite-plugin-i18next-loader';
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [commonjs(), i18nextLoader({ paths: ['./src/locales'], namespaceResolution: 'basename' }), react()],
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, 'src')
-    }
-  },
-  worker: {
-    plugins: () => [commonjs(), i18nextLoader({ paths: ['./src/locales'], namespaceResolution: 'basename' }), react()],
-  },
-})
+export default defineConfig( {
+	build: {
+		rollupOptions: {
+			input: {
+				app: './create.html',
+			},
+		},
+	},
+	plugins: [
+		commonjs(),
+		i18nextLoader( { paths: [ './src/locales' ], namespaceResolution: 'basename' } ),
+		react(),
+	],
+	resolve: {
+		alias: {
+			'~': path.resolve( __dirname, 'src' ),
+		},
+	},
+	worker: {
+		plugins: () => [
+			commonjs(),
+			i18nextLoader( { paths: [ './src/locales' ], namespaceResolution: 'basename' } ),
+			react(),
+		],
+	},
+	server: {
+		open: '/create.html',
+	},
+} );
