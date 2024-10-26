447d8c285fee414294776ca94ae27d97262fa2dd
Igor Klimer <github@gpg.klimer.eu>
2024-10-26 22:22:27 +0000

    Add explicit support for Supernote A5 X
    Looks like it has exactly the same hardware specifications (at least for
    the resolution and DPI) as rM 1&2.
    
    Fixes #14

--- src/lib/device-utils.js
+++ src/lib/device-utils.js
@@ -1,8 +1,14 @@
 export const REMARKABLE = 'ReMarkable 1 & 2';
 const REMARKABLE_PAPER_PRO = 'ReMarkable Paper Pro';
+const SUPERNOTE_A5_X = 'Supernote A5 X';
 export const CUSTOM = 'Custom';
 
-export const AVAILABLE_DEVICES = [ REMARKABLE, REMARKABLE_PAPER_PRO, CUSTOM ];
+export const AVAILABLE_DEVICES = [
+	REMARKABLE,
+	REMARKABLE_PAPER_PRO,
+	SUPERNOTE_A5_X,
+	CUSTOM,
+];
 
 export function getPageProperties( device ) {
 	switch ( device ) {
@@ -12,8 +18,9 @@ export function getPageProperties( device ) {
 				pageSize: [ 1620, 2160 ],
 			};
 
-		default:
+		case SUPERNOTE_A5_X:
 		case REMARKABLE:
+		default:
 			return {
 				dpi: 226,
 				pageSize: [ 1404, 1872 ],
