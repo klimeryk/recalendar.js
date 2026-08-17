0171237cf7e1a453231feab5fa64b1629917ca34
Igor Klimer <github@gpg.klimer.eu>
2026-08-17 22:42:06 +0000

    Add support for new Supernote devices and RM Pro Move
    Props to @hiltonbrown and https://github.com/klimeryk/recalendar.js/pull/148

--- src/lib/device-utils.js
+++ src/lib/device-utils.js
@@ -1,12 +1,18 @@
 export const REMARKABLE = 'ReMarkable 1 & 2';
 const REMARKABLE_PAPER_PRO = 'ReMarkable Paper Pro';
+const REMARKABLE_PAPER_PRO_MOVE = 'ReMarkable Paper Pro Move';
 const SUPERNOTE_A5_X = 'Supernote A5 X';
+const SUPERNOTE_NOMAD = 'Supernote Nomad';
+const SUPERNOTE_MANTA = 'Supernote Manta';
 export const CUSTOM = 'Custom';
 
 export const AVAILABLE_DEVICES = [
 	REMARKABLE,
 	REMARKABLE_PAPER_PRO,
+	REMARKABLE_PAPER_PRO_MOVE,
 	SUPERNOTE_A5_X,
+	SUPERNOTE_NOMAD,
+	SUPERNOTE_MANTA,
 	CUSTOM,
 ];
 
@@ -16,6 +22,24 @@ export function getPageProperties( device ) {
 			return {
 				dpi: 229,
 				pageSize: [ 1620, 2160 ],
+			};
+
+		case REMARKABLE_PAPER_PRO_MOVE:
+			return {
+				dpi: 229,
+				pageSize: [ 1696, 954 ],
+			};
+
+		case SUPERNOTE_NOMAD:
+			return {
+				dpi: 226,
+				pageSize: [ 1404, 1872 ],
+			};
+
+		case SUPERNOTE_MANTA:
+			return {
+				dpi: 300,
+				pageSize: [ 1920, 2560 ],
 			};
 
 		case SUPERNOTE_A5_X:
